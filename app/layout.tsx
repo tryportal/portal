6617c9d3dc90755a6bbbe1ea22ef85bfce3e7b3c
SALT <abdquy@gmail.com>
2026-01-14 15:43:23 +0800

    Fix favicon: remove DynamicFavicon override and use favicon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,6 @@ import { ClerkThemeProvider } from "@/lib/clerk-theme-provider";
 import { UserSettingsProvider } from "@/lib/user-settings";
 import { RootNotificationProvider } from "@/components/notifications/notification-provider";
 import { CookieConsentBanner } from "@/components/cookie-consent-banner";
-import { DynamicFavicon } from "@/components/dynamic-favicon";
 import { Toaster } from "sonner";
 import "./globals.css";
 
@@ -115,7 +114,6 @@ export default function RootLayout({
         </head>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <ThemeProvider defaultTheme="system" storageKey="portal-theme">
-            <DynamicFavicon />
             <ClerkThemeProvider>
               <UserSettingsProvider>
                 <PostHogProvider>
